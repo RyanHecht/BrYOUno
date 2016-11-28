1b67469cbf1d883f7bbfc8b1a9de652aeb7a6282
BuildTools <ryanhecht.tse@gmail.com>
2016-11-27 23:52:57 -0500

    added second input

--- lightArduino/lightArduino.ino
+++ lightArduino/lightArduino.ino
@@ -1,17 +1,21 @@
 
 // Pin declaration
-const int  btnPin = A0; 
+const int btnPin = A0; // manual button trigger
+const int remPin = A1; // pin for remote trigger 
 const int servoPin1 = 5;
 const int servoPin2 = 9;
 
 // state variables
 int btnState = 0;        
-int oldBtnState = 0;    
+int oldBtnState = 0;
+int remState = 0;
+int oldRemState = 0;    
 int count = 0;
 
 void setup() {
   // init pin modes
   pinMode(btnPin, INPUT);
+  pinMode(remPin, INPUT);
   pinMode(servoPin1, OUTPUT);
   pinMode(servoPin2, OUTPUT);
 
@@ -23,7 +27,7 @@ void setup() {
 
 void loop() {
   btnState = digitalRead(btnPin);
-
+  remState = digitalRead(remPin);
   if (btnState != oldBtnState) {
     // If the state has changed from low to high, we should increment the counter
     if (btnState == HIGH) {
@@ -37,17 +41,30 @@ void loop() {
         digitalWrite(servoPin1, LOW);
         digitalWrite(servoPin2, HIGH);
       }
+      //set cooldown of one second
+      delay(1000);
     }
-    //set cooldown of one second
-    delay(1000);
   }
 
+  if (remState != oldRemState) {
+    if (remState == HIGH) {
+      count++;
+      // change state according to the counter
+      if (count % 2 == 0) {
+        digitalWrite(servoPin1, HIGH);
+        digitalWrite(servoPin2, LOW);
+      } 
+      else {
+        digitalWrite(servoPin1, LOW);
+        digitalWrite(servoPin2, HIGH);
+      }
+      //set cooldown of one second
+      delay(1000); 
+    }
+  }
   // save this state as the old one for later
   oldBtnState = btnState;
-
-
-  
-
-
+  oldRemState = remState;
 }
+
 
