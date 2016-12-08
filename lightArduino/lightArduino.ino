1ec7a0db3e9f97ac591839f7d31eb82242b484a7
RyanHecht <ryanhecht.tse@gmail.com>
2016-12-08 12:51:14 -0500

    extensive arduino script minification, fixed an issue where lights would change states twice sometimes

--- lightArduino/lightArduino.ino
+++ lightArduino/lightArduino.ino
@@ -3,16 +3,13 @@
 
 // Pin declaration
 const int btnPin = A0; // manual button trigger
-const int remPin = A4; // pin for remote trigger 
+const int remPin = A1; // pin for remote trigger 
 const int servoPin1 = 5;
 const int servoPin2 = 9;
 
 // state variables
 int btnState = 0;        
-int oldBtnState = 0;
-int remState = 0;
-int oldRemState = 0;    
-int count = 0;
+int remState = 0;   
 
 void setup() {
   // init pin modes
@@ -20,53 +17,23 @@ void setup() {
   pinMode(remPin, INPUT);
   pinMode(servoPin1, OUTPUT);
   pinMode(servoPin2, OUTPUT);
-
-  // initialize servos as opposing, as they'll be on opposite sides of the switch
-  digitalWrite(servoPin1, HIGH);
-  digitalWrite(servoPin2, LOW);
 }
 
 
 void loop() {
+  // query states of inputs
   btnState = digitalRead(btnPin);
   remState = digitalRead(remPin);
-  if (btnState != oldBtnState) {
-    // If the state has changed from low to high, we should increment the counter
-    if (btnState == HIGH) {
-      count++;
-      // change state according to the counter
-      if (count % 2 == 0) {
-        digitalWrite(servoPin1, HIGH);
-        digitalWrite(servoPin2, LOW);
-      } 
-      else {
-        digitalWrite(servoPin1, LOW);
-        digitalWrite(servoPin2, HIGH);
-      }
-      //set cooldown of one second
-      delay(1000);
-    }
-  }
 
-  if (remState != oldRemState) {
-    if (remState == HIGH) {
-      count++;
-      // change state according to the counter
-      if (count % 2 == 0) {
-        digitalWrite(servoPin1, HIGH);
-        digitalWrite(servoPin2, LOW);
-      } 
-      else {
-        digitalWrite(servoPin1, LOW);
-        digitalWrite(servoPin2, HIGH);
-      }
-      //set cooldown of one second
-      delay(1000); 
-    }
+  // If either of them are high...
+  if(btnState == HIGH || remState == HIGH) {
+    // let's change the state of the servo pins
+    digitalWrite(servoPin1, !digitalRead(servoPin1));
+    digitalWrite(servoPin2, !digitalRead(servoPin2));
+
+    // sleep for two seconds to prevent spam
+    delay(2000);
   }
-  // save this state as the old one for later
-  oldBtnState = btnState;
-  oldRemState = remState;
 }
 
 
